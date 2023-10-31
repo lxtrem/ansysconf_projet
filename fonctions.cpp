de8e3beda9e99c754d75af9812e97e1abb5a315f
lxtrem <33664866+lxtrem@users.noreply.github.com>
2023-10-31 13:14:16 +0100

    Update fonctions.cpp

--- fonctions.cpp
+++ fonctions.cpp
@@ -1,17 +1,5 @@
 #include "fonctions.hpp"
 
-/*template <typename T>
-bool initListe(std::vector<std::string> *&listeNoms, std::map<std::string,T> *&listeStructs, const rapidjson::Value& obj)
-{
-    if (!obj.IsObject()) {return false;}
-    std::string nom;
-    for (int i{0}; i < obj["types"].Size(); i++) {
-        nom = obj["types"][i]["nom"].GetString();
-        listeStructs->insert({nom, T(obj["types"][i])});
-        listeNoms->push_back(nom);
-    }
-    return true;
-}*/
 rapidjson::Document getData(std::string filename) {
     std::ifstream json;
     json.open(filename);
